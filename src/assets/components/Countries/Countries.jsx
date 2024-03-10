0f56af6af9cd79a3392dd5d76bb6fe89764b3e85
tofayeLL <ahmedtofayel132@gmail.com>
2024-03-10 21:32:22 +0600

    added visited button and toggle the button

--- src/assets/components/Countries/Countries.jsx
+++ src/assets/components/Countries/Countries.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -13,9 +14,11 @@ const Countries = () => {
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
-            {
-                countries.map(country => <Country country={country} key={country.cca3}></Country>)
-            }
+            <div className="countries-container">
+                {
+                    countries.map(country => <Country country={country} key={country.cca3}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/assets/components/Country/Country.jsx
+++ src/assets/components/Country/Country.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+const Country = ({ country }) => {
     console.log(country);
-    const {name, flags} = country;
+    const { name, flags, population, area, cca3 } = country;
+    const [visited, setVisited] = useState(false);
+    const handleVisited = () => {
+        setVisited(!visited);
+
+    }
     return (
         <div className="style">
             <h3>country: {name?.common}</h3>
             <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p><small>code: {cca3}</small></p>
+            <button onClick={handleVisited}>{visited ? 'visited' : 'going'}</button>
+            {visited ? 'visited' : 'I want to visit'}
 
-            
+
         </div>
     );
 };
